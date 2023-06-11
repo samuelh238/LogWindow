21164d1f5cb87c20b8cc0e7d75c32fe747ba028b
samuelh238 <samuelh238@gmail.com>
2023-06-11 08:33:00 -0500

    Added context menu with saving to text or log file

--- LogWindow/UserControl1.xaml.cs
+++ LogWindow/UserControl1.xaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
@@ -33,6 +34,7 @@ namespace LogWindow
             string? debugTag = Enum.GetName(typeof(LogLevels), colorCode);
             var run = new Run() { Text = $">{debugTag}: {logmessage}\r\n", Foreground = brushColor };
             _paragraph.Inlines.Add(run);
+            this.rtbLogWindow.ScrollToEnd();
         }
 
         private Brush GetBrushColorFromLogLevel(LogLevels colorCode)
@@ -52,6 +54,51 @@ namespace LogWindow
                 default:
                     return Brushes.White;
             }
+        }
+        private void miSaveLogToFile_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Configure save file dialog box
+            var dialog = new Microsoft.Win32.SaveFileDialog();
+            //dialog.FileName = $""; // Default file name
+            dialog.DefaultExt = ".txt"; // Default file extension
+            dialog.Filter = "Text documents (.txt)|*.txt|Log Files (.log)|*.log"; // Filter files by extension
+
+            // Show save file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process save file dialog box results
+            if (result != true)
+                return;
+            // Save document
+            string filename = dialog.FileName;
+            this.DisplayLogMessage($"Saving Log to File: {filename}", LogLevels.INFO);
+            try
+            {
+                StreamWriter outputFile = new StreamWriter(filename);
+                outputFile.Write(StringFromRichTextBox());
+                outputFile.Close();
+                this.DisplayLogMessage($"Log successfully saved to File: {filename}", LogLevels.SUCCESS);
+            }
+            catch (Exception ex)
+            {
+                this.DisplayLogMessage($"Failed to write log to File: {filename}", LogLevels.ERROR);
+                this.DisplayLogMessage($"{ex.Message}", LogLevels.ERROR);
+                this.DisplayLogMessage($"{ex.StackTrace}", LogLevels.DEBUG);
+            }
+        }
+
+        private string StringFromRichTextBox()
+        {
+            TextRange textRange = new TextRange(
+                // TextPointer to the start of content in the RichTextBox.
+                this.rtbLogWindow.Document.ContentStart,
+                // TextPointer to the end of content in the RichTextBox.
+                this.rtbLogWindow.Document.ContentEnd
+            );
+
+            // The Text property on a TextRange object returns a string
+            // representing the plain text content of the TextRange.
+            return textRange.Text;
         }
     }
 }
